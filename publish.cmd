c3b30c8220f64f59dc0061a071c2c223e3594588
pokor123 <36666489+pokor123@users.noreply.github.com>
2018-06-06 10:34:43 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qnabotai-Web-Deploy.pubxml -p:Password=aelT2yisnk0qfsNevoFsw3tAbRkx8XKaqoe0MKzRskmE6nfAASvwZZrGM6EG
+
